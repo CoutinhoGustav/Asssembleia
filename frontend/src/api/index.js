b39b17e5ebbdd7c3bf749964b0015950b26ff87b
Gustavo Coutinho <94051827+CoutinhoGustav@users.noreply.github.com>
2026-02-24 20:10:26 -0300

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://asssembleia.vercel.app/',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3002',
 });
 console.log('API Base URL:', api.defaults.baseURL);
 
