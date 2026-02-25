577bf5591cdae939dc0c4fbfdbe4311f336cdd99
Gustavo Coutinho <gustavocoutinho32@gmail.com>
2026-02-25 00:42:31 -0300

    atualização

--- backend/models/SystemStatus.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const systemStatusSchema = new mongoose.Schema({
-    id: { type: String, default: 'main' },
-    isCallActive: { type: Boolean, default: false },
-});
-
-module.exports = mongoose.model('SystemStatus', systemStatusSchema);
